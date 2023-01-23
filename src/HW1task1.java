bf550f2aa17c638adb6b527a434b77e1f2e8bcd9
Semiranov <semiranov@gmail.com>
2023-01-24 01:08:25 +0300

    add HW1

--- src/HW1task1.java
+++ src/HW1task1.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class HW1task1 {
+
+    private static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("eee");
+        int num = getNum("Введите число N: ");
+        int rez = countingNumber(num);
+        printRez(rez);
     }
+
+
+    static int getNum(String text) { //Запрос и ввод числа
+        System.out.print(text);
+        return input.nextInt();
+    }
+
+    static int countingNumber(int num) { //Подсчет числа
+        return num * (num + 1) / 2;
+    }
+
+    static void printRez(int num) {  //Вывод числа на экран
+        System.out.println("Треугольное число = " + num);
+    }
+
 }
+
+
+//1.Спросить ввод числа н
+//1.1 Вычислить число
+//1.2 Вывести число на экран
